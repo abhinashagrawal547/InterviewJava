2600405a6e822fbd0f495312763ac5e008cc0bcc
Abhinash <abhinashagrawal547@gmail.com>
2023-04-27 16:04:28 +0530

    updating to latest 2

---
+++ src/main/java/ArmstrongNumber.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String inputNoStr = sc.nextLine();
+		int inputNo = Integer.parseInt(inputNoStr);
+		int backUpNo = inputNo;
+		int remainder = 1;
+		int sumOfEachNo = 0;
+		while(inputNo!=0) {
+			remainder = inputNo%10;
+			sumOfEachNo = sumOfEachNo + (remainder * remainder * remainder);
+			inputNo = inputNo/10;
+		}
+		
+		if(sumOfEachNo==backUpNo)
+			System.out.println(backUpNo + " is a armstrong number");
+		else
+			System.out.println(backUpNo + " is not a armstrong number");
+			
+	}
+
+}
---
+++ src/main/java/Palindrome.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		int inputInt = Integer.parseInt(input);
+		int copyOfInput = inputInt;
+		boolean isPalindrome = true;
+		
+		ArrayList<Integer> inputArr = new ArrayList<Integer>(); 
+		
+		while(inputInt%10 != 0) {
+			int remainder = inputInt%10;
+			inputArr.add(remainder);
+			inputInt = inputInt / 10;
+		}
+		
+		System.out.println(inputArr);
+		
+		for(int i=0; i<inputArr.size() / 2; i++) {
+			if(inputArr.get(i) != inputArr.get(inputArr.size() - i - 1)) {
+				System.out.println(input + " is not a palindrome");
+				isPalindrome = false;
+				break;
+			}
+		}
+		
+		if(isPalindrome == true)
+			System.out.println(input + " is a palindrome");
+	}
+
+}
