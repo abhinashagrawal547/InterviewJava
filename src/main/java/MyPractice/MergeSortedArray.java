b12aade2ca02f958d4d18f0fd4aacfde951617d4
ABHINASH <abhinashagrawal547@gmail.com>
2025-04-10 14:03:06 -0500

    added a new code as practice done today 10 April '25

---
+++ src/main/java/MyPractice/MergeSortedArray.java
@@ -0,0 +1,16 @@
+package MyPractice;
+
+public class MergeSortedArray {
+    public static void main(String[] args) {
+        int[] num1 = {1,2,3,0,0,0};
+        int[] num2 = {2,5,6};
+        int m = 3;
+        int n = 3;
+
+        int[] result = new int[m+n];
+
+        int i=0,j=0,k=0;
+
+
+    }
+}
